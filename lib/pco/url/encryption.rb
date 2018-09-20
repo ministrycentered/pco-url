03dc59823f6ba992499bef8a799e2906d9b956a5
Daniel Ma <drailskid@yahoo.com>
2018-09-20 10:59:19 -0700

    feat(Encryption): pass rubocop & new API
    Update encryption to be thread safe, based on 2 constraints
    
    1. Module throws an error if anyone attempts to change the key
    2. Specific key can be passed into `encrypt` and `decrypt` for
       specific cases

--- lib/pco/url/encryption.rb
+++ lib/pco/url/encryption.rb
@@ -21,87 +21,102 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require 'openssl'
+require "openssl"
 
 module PCO
   class URL
     module Encryption
+      class DecryptError < ::ArgumentError; end
+      class MissingKeyError < ::ArgumentError; end
+
       # avoid vowels to not generate four-letter words, etc.
-      # this is important because those words can trigger spam 
+      # this is important because those words can trigger spam
       # filters when URLs are used in emails
       TABLE = "1bcd2fgh3jklmn4pqrstAvwxyz567890".freeze
-
-      def self.key=(key)
-        @key = key
-      end
 
       class Chunk
         def initialize(bytes)
           @bytes = bytes
         end
 
-        def decode
-          bytes = @bytes.take_while {|c| c != 61} # strip padding
-          bytes = bytes.find_all{|b| !TABLE.index(b.chr).nil? } # remove invalid characters
+        def decode # rubocop:disable Metrics/AbcSize
+          bytes = @bytes.take_while { |c| c != 61 } # strip padding
+          bytes = bytes.find_all { |b| !TABLE.index(b.chr).nil? } # remove invalid characters
           n = (bytes.length * 5.0 / 8.0).floor
           p = bytes.length < 8 ? 5 - (n * 8) % 5 : 0
-          c = bytes.inject(0) {|m,o| (m << 5) + TABLE.index(o.chr)} >> p
-          (0..n-1).to_a.reverse.collect {|i| ((c >> i * 8) & 0xff).chr}
+          c = bytes.inject(0) { |m, o| (m << 5) + TABLE.index(o.chr) } >> p
+          (0..(n - 1)).to_a.reverse.collect { |i| ((c >> i * 8) & 0xff).chr }
         end
 
-        def encode
+        def encode # rubocop:disable Metrics/AbcSize
           n = (@bytes.length * 8.0 / 5.0).ceil
           p = n < 8 ? 5 - (@bytes.length * 8) % 5 : 0
-          c = @bytes.inject(0) {|m,o| (m << 8) + o} << p
-          [(0..n-1).to_a.reverse.collect {|i| TABLE[(c >> i * 5) & 0x1f].chr},
-           ("=" * (8-n))] # TODO: remove '=' padding generation
+          c = @bytes.inject(0) { |m, o| (m << 8) + o } << p
+          [
+            (0..(n - 1)).to_a.reverse.collect { |i| TABLE[(c >> i * 5) & 0x1f].chr },
+            ("=" * (8 - n))
+          ]
         end
-        
       end
 
-      def self.chunks(str, size)
-        result = []
-        bytes = str.bytes
-        while bytes.any? do
-          result << Chunk.new(bytes.take(size))
-          bytes = bytes.drop(size)
+      class << self
+        def default_key=(key)
+          raise(<<~ERR) if defined?(@default_key) && @default_key != key
+            Don't change the default_key during the life of a process.
+            This configuration variable is meant to be set once on boot and not
+            touched again.
+
+            If your app needs to use multiple keys, pass them in explicitly to `encrypt`
+            and `decrypt`
+          ERR
+
+          @default_key = key
         end
-        result
-      end
 
-      # strip '=' padding, because we don't need it
-      def self.encode(data)
-        chunks(data, 5).collect(&:encode).flatten.join.tr('=','')
-      end
+        def decrypt(data, key: @default_key)
+          raise MissingKeyError, "default key or key: argument must be set" if key.nil?
+          iv, encrypted = data.split("Z").map { |part| decode(part) }
+          raise DecryptError, "not a valid string to decrypt" unless iv && encrypted
+          decrypter = cipher(:decrypt, key: key)
+          decrypter.iv = iv
+          decrypter.update(encrypted) + decrypter.final
+        end
 
-      def self.decode(data)
-        chunks(data, 8).collect(&:decode).flatten.join
-      end
-      
-      def self.decrypt(data)
-        iv, encrypted = data.split('Z').map{|part| decode(part)}
-        fail DecryptError, "not a valid string to decrypt" unless iv && encrypted
-        decrypter = cipher(:decrypt)
-        decrypter.iv = iv
-        decrypter.update(encrypted) + decrypter.final 
-      end
-      
-      def self.encrypt(data)
-        crypter = cipher(:encrypt)
-        crypter.iv = iv = crypter.random_iv
-        "#{encode(iv)}Z#{encode(crypter.update(data) + crypter.final)}"
-      end
-      
-      private 
-      
-      def self.cipher(mode)
-        cipher = OpenSSL::Cipher.new('aes-256-cbc')
-        cipher.send(mode)
-        cipher.key = @key
-        cipher
-      end
+        def encrypt(data, key: @default_key)
+          raise MissingKeyError, "default key or key: argument must be set" if key.nil?
+          crypter = cipher(:encrypt, key: key)
+          crypter.iv = iv = crypter.random_iv
+          "#{encode(iv)}Z#{encode(crypter.update(data) + crypter.final)}"
+        end
 
-      class DecryptError < ::ArgumentError; end
+        private
+
+        def chunks(str, size)
+          result = []
+          bytes = str.bytes
+          while bytes.any?
+            result << Chunk.new(bytes.take(size))
+            bytes = bytes.drop(size)
+          end
+          result
+        end
+
+        # strip "=" padding, because we don"t need it
+        def encode(data)
+          chunks(data, 5).collect(&:encode).flatten.join.tr("=", "")
+        end
+
+        def decode(data)
+          chunks(data, 8).collect(&:decode).flatten.join
+        end
+
+        def cipher(mode, key:)
+          cipher = OpenSSL::Cipher.new("aes-256-cbc")
+          cipher.public_send(mode)
+          cipher.key = key
+          cipher
+        end
+      end
     end
   end
 end
---
+++ spec/pco/url/encryption_spec.rb
@@ -0,0 +1,59 @@
+require "spec_helper"
+
+describe PCO::URL::Encryption do
+  # rubocop:disable Metrics/LineLength
+  TEST_DEFAULT_KEY = "\xE3\x9B\"f\x13\xAD\xDE\xC0(t\x03\x16c\xE2\xE4\x9B\xC7Cj k\xDA\xDB\xA9\xE3\xAB\x96\xE4\xA7`\v\x87".freeze
+  TEST_SPECIFIC_KEY = "A\xA0\xBA\x9FX\xD1\x82\xED\xDB\xD5cx|[\xB3\xE6i\xE7\xA3R\xAF(\xFC\xA4?\xC6\xC3\x7F\xEAu\x90u".freeze
+  # rubocop:enable Metrics/LineLength
+
+  after do
+    described_class.remove_instance_variable(:@default_key) if described_class.instance_variable_defined?(:@default_key)
+  end
+
+  it "gets mad if you try to change the default key" do
+    described_class.default_key = "key!"
+
+    expect { described_class.default_key = "new key!" }.to raise_error(/change the default_key/i)
+  end
+
+  context "without a specific key" do
+    context "when a default key is set" do
+      it "encrypts and decrypts correctly" do
+        described_class.default_key = TEST_DEFAULT_KEY
+        encrypted = described_class.encrypt("BoJack")
+        decrypted = described_class.decrypt(encrypted)
+
+        expect(decrypted).to eq("BoJack")
+      end
+
+      it "decrypts correctly" do
+        described_class.default_key = TEST_DEFAULT_KEY
+        encrypted = "t08dkbvdv0zs27n38AykgbsxlyZpw73jh6vyjrwxy3Akvdqh4xwzm"
+
+        expect(described_class.decrypt(encrypted)).to eq("Horseman")
+      end
+    end
+
+    context "when no default key is set" do
+      it "fails loudly because no key is set" do
+        expect { described_class.encrypt("value") }.to raise_error(described_class::MissingKeyError)
+        expect { described_class.decrypt("value") }.to raise_error(described_class::MissingKeyError)
+      end
+    end
+  end
+
+  context "using a specified key" do
+    it "encrypts and decrypts correctly" do
+      encrypted = described_class.encrypt("You are Secretariat", key: TEST_SPECIFIC_KEY)
+      decrypted = described_class.decrypt(encrypted, key: TEST_SPECIFIC_KEY)
+
+      expect(decrypted).to eq("You are Secretariat")
+    end
+
+    it "decrypts correctly" do
+      encrypted = "3pyb298n27c1k2bcx4r8mAb751Zm12wmAytksfjnq1f4wjdd23dh991k1flr6xqvhtbjlchrnwA4znq"
+
+      expect(described_class.decrypt(encrypted, key: TEST_SPECIFIC_KEY)).to eq("Do the funky Spiderman")
+    end
+  end
+end
